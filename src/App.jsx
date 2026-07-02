81f502d7b50003e5a11fe9349ccd8b79a1d24362
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-07-02 20:48:01 +0100

    React and tailwind css setup, folder structure plans...

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-red-600 p-15 m-20 text-4xl '>i am a robot...</h1>
+    </>
+  )
+}
+
+export default App
